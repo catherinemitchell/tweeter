64f48a89b8529b40eb32edc632eec4aabd0973be
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-30 18:04:30 -0300

    changed div tag to p tag on line 33 and 35

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,73 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
-
-// const renderTweets = function(tweets) {
-//   const $tweetsContainer = $('.tweets-container');
-//   for (let tweet of tweets) {
-//     $tweetsContainer.append(createTweetElement(tweet));
-//   }
-// };
-
-
-// const createTweetElement = function(tweet) {
-//   const tweetHTML = `
-//     <article class="tweet-container">
-//     <div class="tweeted-header">
-//       <div class="header-avatar">
-//         <img src=${tweet.user.avatars}>
-//         <span>${tweet.user.name}</span>
-//       </div>
-//       <div>
-//         <span>${tweet.user.handle}</span>
-//       </div>
-//     </div>
-//     <div class="tweet-itself">
-//     <p>${tweet.content.text}</p>
-//     </div>
-//     <footer class="tweeted-footer">
-//       <div>
-//         <span>${tweet.created_at}</span>
-//       </div>
-//       <div>
-//         <i class="fa-solid fa-flag"></i>
-//         <i class="fa-solid fa-retweet"></i>
-//         <i class="fa-solid fa-heart"></i>
-//       </div>
-//     </footer>
-//     </article>
-//     `;
-//   return tweetHTML;
-// };
-
 $(document).ready(function() {
 
 
@@ -81,7 +14,6 @@ $(document).ready(function() {
       $tweetsContainer.prepend(createTweetElement(tweet));
 
     }
-
   };
 
 
@@ -98,9 +30,9 @@ $(document).ready(function() {
           <span>${tweet.user.handle}</span>
         </div>
       </div>
-      <div class="tweet-itself">
+      <p class="tweet-itself">
       ${$("<p>").text(tweet.content.text).html()}
-      </div>
+      </p>
       <footer class="tweeted-footer">
         <div>
           <span>${time}</span>
@@ -120,23 +52,9 @@ $(document).ready(function() {
 
   $('form').on('submit', function(event) {
     event.preventDefault();
-    // const input = $('#tweet-text').val();
-    // console.log(this);
+
 
     const textArea = $(this).find("textarea").val();
-    // console.log("success", textArea);
-    // console.log("textAreaLength", textArea.length)
-
-
-    // if (textArea.length < 1) {
-    //   window.alert("must enter your tweet!");
-    //   return
-    // }
-
-    // if (textArea.length > 140) {
-    //   window.alert("tweet is too long!");
-    //   return
-    // } 
 
     $(".ErrMsg").slideUp();
 
