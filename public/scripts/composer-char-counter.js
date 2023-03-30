d85d57f19aaf47e0c1b8421d61676d346a3a2674
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-30 17:12:50 -0300

    changed code to use jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,37 @@
-$(document).ready(function() {
-  // --- our code goes here ---
+// $(document).ready(function() {
+//   // --- our code goes here ---
   
-$('textarea').on('input', function() {
+// $('textarea').on('input', function() {
   
-  const charCount = 140 - this.value.length
+//   const charCount = 140 - this.value.length
   
-  this.nextElementSibling.children[1].innerText = charCount
+//   this.nextElementSibling.children[1].innerText = charCount
   
+//   if (charCount.length < 0) {
+//     $(".counter").css("color", "red");
+//   } else {
+//     $(".counter").css("color", "black");
+//   } 
   
-})
+// })
 
 
-});
+// });
+
+$(document).ready(function () {
+  // --- our code goes here --- 
+  document
+    .querySelector("#tweet-text")
+    .addEventListener("input", function (event) {
+      let counter = $(this).parent().children().find(".counter");
+      let length = $(this).val().length;
+      let update = $(counter).val(140 - length);
+      update;
+
+      if (update.val() < 0) {
+        $(".counter").css("color", "red");
+      } else {
+        $(".counter").css("color", "black");
+      }
+    });
+}); 
